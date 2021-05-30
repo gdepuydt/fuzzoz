05c3f9be4a27f211ded89240310ca161dac1d023
geert <geert_depuydt@hotmail.com>
2021-05-30 21:38:43 +0200

    PhysSlice should be struct

--- src/mm/physmem.rs
+++ src/mm/physmem.rs
@@ -20,25 +20,31 @@ impl PhysAddr {
 }
 
 /// A consumeable slice of physical memory at `addr` for `size` bytes.
-pub struct PhysSlice(PhysAddr, usize);
+pub struct PhysSlice {
+    addr: PhysAddr, 
+    len: usize,
+}
 
 impl PhysSlice {
     /// Create a new slice to physical memory.
     pub unsafe fn new(addr: PhysAddr, size: usize) -> Self {
-        PhysSlice(addr, size)
+        PhysSlice {
+            addr, 
+            len: size
+        }
     }
 
     /// Get the remaining length of the slice.
     pub fn len(&self) -> usize {
-        self.1
+        self.len
     }
 
     /// Discard bytes from the slice by updating the pointer and length.
     pub fn discard(&mut self, bytes: usize) -> Result<(), ()> {
-        if self.1 >= bytes {
+        if self.len >= bytes {
             // Update the pointer and length.
-            (self.0).0 += bytes as u64; 
-            self.1 -= bytes;
+            (self.addr).0 += bytes as u64; 
+            self.len -= bytes;
             Ok(())
         } else {
             Err(())
@@ -48,16 +54,16 @@ impl PhysSlice {
     /// Read a `T` from the slice, updating the pointer.
     pub unsafe fn consume<T>(&mut self) -> Result<T, ()> {
         // Make sure we have enough data to consume.
-        if self.1 < size_of::<T>() {
+        if self.len < size_of::<T>() {
             return Err(());
         }
 
         // Read the actual data.
-        let data = self.0.read_unaligned::<T>();
+        let data = self.addr.read_unaligned::<T>();
 
         // Update the pointer and length.
-        (self.0).0 += size_of::<T>() as u64; 
-        self.1 -= size_of::<T>();
+        (self.addr).0 += size_of::<T>() as u64; 
+        self.len -= size_of::<T>();
 
         Ok(data)
     }
